5bdf429bb60b89fe4d695a361b89bd0da213f480
Joshua Rosato <joshuarosato@hotmail.com>
2026-06-26 19:32:56 +0000

    Add Nix Index DB and Comma

--- flake.nix
+++ flake.nix
@@ -7,9 +7,11 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, disko, home-manager, nix-index-database, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -18,6 +20,8 @@
         ./disko-config.nix
         ./configuration.nix
         home-manager.nixosModules.home-manager
+        nix-index-database.nixosModules.default
+        { programs.nix-index-database.comma.enable = true; }
       ];
     };
   };
